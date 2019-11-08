d6a5becf1aac1f548aa57bb2e9406445ee3188b0
Felix Palmen <felix@palmen-it.de>
2019-11-08 08:42:26 +0100

    fix line length bug, reorder self-vrfy code

--- tools/mprg2bas.c
+++ tools/mprg2bas.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
 
 #ifdef _WIN32
 # include <io.h>
@@ -52,7 +53,7 @@ int main(int argc, char **argv)
 
     if (argc < 2 || strcmp(argv[1], "-v"))
     {
-        int remaining = 78 - printf("0 fora=%" PRIu16 "to%" PRIu16
+        int remaining = 79 - printf("0 fora=%" PRIu16 "to%" PRIu16
                 ":readb:pokea,b:next:sys%" PRIu16 ":data%u", startaddress,
                 endaddress, startaddress, (unsigned)*r++);
 
@@ -66,7 +67,7 @@ int main(int argc, char **argv)
             if (remaining - needed < 0)
             {
                 ++lineno;
-                remaining = 73-needed;
+                remaining = 74-needed;
                 if (lineno>9) --remaining;
                 if (lineno>99) --remaining;
                 if (lineno>999) --remaining;
@@ -85,18 +86,18 @@ int main(int argc, char **argv)
     }
 
     printf("0 s=5:l=4:for i=%" PRIu16 " to %" PRIu16
-            ":read x:if x>255 then 2\n", startaddress, endaddress+1);
-    puts("1 s=2*s-(s>4095)+x and 8191:poke i,x:next");
+            ":read x\n", startaddress, endaddress+1);
+    puts("1 if x<256 then s=2*s-(s>4095)+x and 8191:poke i,x:next");
     puts("2 if s<>x-256 then print\"error in line\";l:end");
     printf("3 if i<%" PRIu16 " then l=l+1:i=i-1:next\n", endaddress+1);
 
-    int lineno = 4;
     uint16_t sum = 5;
     sum = ((sum << 1) + !!(sum & 4096) + (unsigned)*r) & 8191;
-    int remaining = 78 - printf("4 sys%" PRIu16  ":data%u", startaddress,
+    int remaining = 79 - printf("4 sys%" PRIu16  ":data%u", startaddress,
             (unsigned)*r++);
-    --nread;
 
+    --nread;
+    int lineno = 4;
     while (nread--)
     {
         uint16_t nsum = ((sum << 1) + !!(sum & 4096) + (unsigned)*r) & 8191;
@@ -108,12 +109,13 @@ int main(int argc, char **argv)
         if (remaining - needed < 0)
         {
             printf(",%u\n", sum+256);
-            remaining = 73 - (needed - nsumlen);
+	    ++lineno;
+            remaining = 74 - (needed - nsumlen);
             if (lineno>9) --remaining;
             if (lineno>99) --remaining;
             if (lineno>999) --remaining;
             if (lineno>9999) --remaining;
-            printf("%d data%u", ++lineno, (unsigned)*r++);
+            printf("%d data%u", lineno, (unsigned)*r++);
         }
         else
         {
