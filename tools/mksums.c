32bccd255b9fdc6636009e47163b36aa22747ecd
Felix Palmen <felix@palmen-it.de>
2019-12-06 09:11:38 +0100

    workaround for alternative PETSCII codes

--- tools/mksums.c
+++ tools/mksums.c
@@ -14,12 +14,14 @@
 #define MAXLINES 16*1024
 
 unsigned char buf[BUFSIZE];
-uint16_t warnlines[MAXLINES];
+uint16_t warnssp[MAXLINES];
+uint16_t warnaltp[MAXLINES];
 
 int main(int argc, char **argv)
 {
     int optsok = 1;
     int allowshiftspace = 0;
+    int allowaltpetscii = 0;
     int skiprem = 0;
 
     for (int i = 1; i < argc; ++i)
@@ -36,6 +38,10 @@ int main(int argc, char **argv)
             {
                 case 's':
                     allowshiftspace = 1;
+                    break;
+
+                case 'p':
+                    allowaltpetscii = 1;
                     break;
 
                 case 'r':
@@ -73,34 +79,34 @@ argsdone:
 
     switch (startaddress)
     {
-	case 0x0801:
-	    fputs("BASIC program at $0801 (C64)\n", stderr);
-	    break;
+        case 0x0801:
+            fputs("BASIC program at $0801 (C64)\n", stderr);
+            break;
 
-	case 0x1001:
-	    fputs("BASIC program at $1001 (C16/116/+4 or VIC20)\n", stderr);
-	    break;
-	
-	case 0x1201:
-	    fputs("BASIC program at $1201 (VIC20 /w memory expansion)\n",
-		    stderr);
-	    break;
+        case 0x1001:
+            fputs("BASIC program at $1001 (C16/116/+4 or VIC20)\n", stderr);
+            break;
 
-	case 0x1c01:
-	    fputs("BASIC program at $1C01 (C128)\n", stderr);
-	    break;
+        case 0x1201:
+            fputs("BASIC program at $1201 (VIC20 /w memory expansion)\n",
+                    stderr);
+            break;
 
-	case 0x4001:
-	    fputs("BASIC program at $4001 (C128 /w bitmap gfx)\n", stderr);
-	    break;
+        case 0x1c01:
+            fputs("BASIC program at $1C01 (C128)\n", stderr);
+            break;
 
-	default:
-	    fputs("!!!!!!!! WARNING !!!!!!!!\n", stderr);
-	    fprintf(stderr, "Load address is $%04X, this is unusual!\n",
-		    (unsigned)startaddress);
-	    fputs("The following output may be bogus, make sure this is "
-		    "indeed a BASIC PRG file\nfor one of the supported "
-		    "systems (VIC-20, C64, C16/116/+4 or C128)!\n\n", stderr);
+        case 0x4001:
+            fputs("BASIC program at $4001 (C128 /w bitmap gfx)\n", stderr);
+            break;
+
+        default:
+            fputs("!!!!!!!! WARNING !!!!!!!!\n", stderr);
+            fprintf(stderr, "Load address is $%04X, this is unusual!\n",
+                    (unsigned)startaddress);
+            fputs("The following output may be bogus, make sure this is "
+                    "indeed a BASIC PRG file\nfor one of the supported "
+                    "systems (VIC-20, C64, C16/116/+4 or C128)!\n\n", stderr);
     }
 
     size_t nread = fread(buf, 1, BUFSIZE, stdin);
@@ -118,56 +124,58 @@ argsdone:
         bufptr += nchunk;
         rembuf -= nchunk;
         nchunk = fread(bufptr, 1, rembuf, stdin);
+        int allowaltpetscii = 0;
         nread += nchunk;
     }
 
     size_t pos = 0;
     int retval = 0;
-    size_t warnlinesnum = 0;
+    size_t warnsspnum = 0;
+    size_t warnaltpnum = 0;
 
     fflush(stderr);
     for (;;)
     {
-	if (pos > nread-2)
-	{
-	    retval = 1;
-	    goto end;
-	}
-	if (!buf[pos] && !buf[pos+1]) goto end;
+        if (pos > nread-2)
+        {
+            retval = 1;
+            goto end;
+        }
+        if (!buf[pos] && !buf[pos+1]) goto end;
 
-	pos += 2;
+        pos += 2;
 
-	if (pos > nread-2)
-	{
-	    retval = 1;
-	    goto end;
-	}
+        if (pos > nread-2)
+        {
+            retval = 1;
+            goto end;
+        }
 
-	uint16_t lineno = 0;
-	lineno += buf[pos++];
-	lineno += buf[pos++] << 8;
+        uint16_t lineno = 0;
+        lineno += buf[pos++];
+        lineno += buf[pos++] << 8;
 
-	printf("%u:\t", (unsigned)lineno);
+        printf("%u:\t", (unsigned)lineno);
 
-	uint16_t lfsr = lineno ^ 0xffff;
-	int inquot = 0;
-	unsigned char c;
-	while ((c = buf[pos++]))
-	{
-	    if (pos > nread)
-	    {
-		retval = 1;
-		goto end;
-	    }
-	    if (c==0xa0 && !allowshiftspace)
-	    {
-		if (warnlinesnum < MAXLINES)
-		{
-		    warnlines[warnlinesnum++] = lineno;
-		}
-		c = ' ';
-	    }
-	    if (!inquot && c==' ') continue;
+        uint16_t lfsr = lineno ^ 0xffff;
+        int inquot = 0;
+        unsigned char c;
+        while ((c = buf[pos++]))
+        {
+            if (pos > nread)
+            {
+                retval = 1;
+                goto end;
+            }
+            if (c==0xa0 && !allowshiftspace)
+            {
+                if (warnsspnum < MAXLINES)
+                {
+                    warnssp[warnsspnum++] = lineno;
+                }
+                c = ' ';
+            }
+            if (!inquot && c==' ') continue;
             if (!inquot && skiprem)
             {
                 if (c==':')
@@ -180,38 +188,65 @@ argsdone:
                     }
                 }
             }
-	    if (c=='"') inquot = !inquot;
-	    for (int b = 0; b < 8; ++b)
-	    {
-		unsigned char tmp = !!(c & 0x80);
-		c <<= 1;
-		tmp ^= (lfsr & 1);
-		lfsr >>= 1;
-		if (tmp) lfsr ^= 0xb400;
-	    }
-	}
-	printf("%04X\n", (unsigned)lfsr);
+            if (inquot && !allowaltpetscii)
+            {
+                if ((c >= 0x60 && c < 0x80) || c == 0xde)
+                {
+                    if (warnaltpnum < MAXLINES)
+                    {
+                        warnaltp[warnaltpnum++] = lineno;
+                    }
+                    if (c == 0xde || c == 0x7e) c= 0xff;
+                    else c += 0x60;
+                }
+            }
+            if (c=='"') inquot = !inquot;
+            for (int b = 0; b < 8; ++b)
+            {
+                unsigned char tmp = !!(c & 0x80);
+                c <<= 1;
+                tmp ^= (lfsr & 1);
+                lfsr >>= 1;
+                if (tmp) lfsr ^= 0xb400;
+            }
+        }
+        printf("%04X\n", (unsigned)lfsr);
     }
 
 end:
-    if (warnlinesnum)
+    if (warnsspnum)
     {
-	fputs("!!!!!!!! WARNING !!!!!!!!\n", stderr);
-	fputs("Shifted spaces found on line(s)", stderr);
-	for (size_t i = 0; i < warnlinesnum; ++i)
-	{
-	    fputc(" ,"[!!i], stderr);
-	    fprintf(stderr, "%u", (unsigned)warnlines[i]);
-	}
-	fputs(".\n", stderr);
-	fputs("They were automatically replaced by normal spaces.\n", stderr);
-	fputs("If this program really needs shifted spaces, use the -s "
-	       "option.\n", stderr);
+        fputs("!!!!!!!! WARNING !!!!!!!!\n", stderr);
+        fputs("Shifted spaces found on line(s)", stderr);
+        for (size_t i = 0; i < warnsspnum; ++i)
+        {
+            fputc(" ,"[!!i], stderr);
+            fprintf(stderr, "%u", (unsigned)warnssp[i]);
+        }
+        fputs(".\n", stderr);
+        fputs("They were automatically replaced by normal spaces.\n", stderr);
+        fputs("If this program really needs shifted spaces, use the -s "
+               "option.\n", stderr);
+    }
+
+    if (warnaltpnum)
+    {
+        fputs("!!!!!!!! WARNING !!!!!!!!\n", stderr);
+        fputs("Alternative PETSCII codes found on line(s)", stderr);
+        for (size_t i = 0; i < warnaltpnum; ++i)
+        {
+            fputc(" ,"[!!i], stderr);
+            fprintf(stderr, "%u", (unsigned)warnaltp[i]);
+        }
+        fputs(".\n", stderr);
+        fputs("They were automatically replaced by canonical codes.\n", stderr);
+        fputs("If this program really needs alternative codes, use the -p "
+               "option.\n", stderr);
     }
 
     if (retval)
     {
-	fputs("Unknown error parsing BASIC program\n", stderr);
+        fputs("Unknown error parsing BASIC program\n", stderr);
     }
     return retval;
 }
